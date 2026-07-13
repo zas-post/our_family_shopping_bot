e394f8f7d6b97dbbf73e2c662eb7391bd7e96e11
Alexander <a.zabolev@yadro.com>
2026-07-13 16:17:45 +0300

    Upd

--- handlers/common.py
+++ handlers/common.py
@@ -4,11 +4,12 @@ from aiogram.filters import Command
 
 router = Router()
 
+
 @router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer(
         "Привет! Я Твой Семейный список покупок. 🛒\n\n"
-        "**Как мной пользоваться:**\n"
+        "<b>Как мной пользоваться:</b>\n"
         "• Добавить товары: `п+ Название 2шт`\n"
         "• Показать и закрепить список заново: /list или /pin\n\n"
         "Под списком есть кнопка `🏃‍♂️ Я в магазине!`, нажмите её, чтобы предупредить семью!"
